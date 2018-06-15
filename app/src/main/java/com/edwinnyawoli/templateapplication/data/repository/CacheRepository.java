7d554ce9aec4fe69a5c57d88a066e6df43755c77
Edwin Nyawoli <edwin.nyawoli@gmail.com>
2018-06-15 19:53:23 +0000

    Add CacheRepo

---
+++ app/src/main/java/com/edwinnyawoli/templateapplication/data/repository/CacheRepository.java
@@ -0,0 +1,32 @@
+package com.edwinnyawoli.templateapplication.data.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ *
+ */
+
+@Singleton
+public class CacheRepository {
+
+    private Map<String, Object> cache;
+
+    @Inject
+    public CacheRepository() {
+        cache = new HashMap<>();
+    }
+
+    public void put(String key, Object object) {
+        cache.put(key, object);
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key) {
+        return (T) cache.get(key);
+    }
+
+}
