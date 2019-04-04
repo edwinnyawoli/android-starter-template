3ab8d9cd91b070747d820ab648ea46ac56132e0c
Edwin Nyawoli <edwin.nyawoli@gmail.com>
2019-04-04 15:21:55 +0000

    Add an empty activity and a fullscreen activity

--- app/src/main/java/com/edwinnyawoli/templateapplication/di/module/ActivityModule.java
+++ app/src/main/java/com/edwinnyawoli/templateapplication/di/module/ActivityModule.java
@@ -7,6 +7,6 @@ import dagger.Module;
  */
 
 @Module
-public class ActivityModule {
+public abstract class ActivityModule {
 
 }
--- app/src/main/java/com/edwinnyawoli/templateapplication/ui/SplashActivity.java
+++ app/src/main/java/com/edwinnyawoli/templateapplication/ui/SplashActivity.java
@@ -3,10 +3,13 @@ package com.edwinnyawoli.templateapplication.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import java.util.UUID;
+
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        startActivity(EmptyActivity.intentFor(this, UUID.randomUUID().toString()));
     }
 }
