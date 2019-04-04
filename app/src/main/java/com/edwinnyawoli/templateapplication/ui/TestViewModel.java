0e690b1cba5516fd99dbf189b0d894fcd9e4f45c
Edwin Nyawoli <edwin.nyawoli@gmail.com>
2019-04-04 15:25:43 +0000

    Add a test ViewModel class

---
+++ app/src/main/java/com/edwinnyawoli/templateapplication/ui/TestViewModel.java
@@ -0,0 +1,15 @@
+package com.edwinnyawoli.templateapplication.ui;
+
+import android.arch.lifecycle.ViewModel;
+
+import javax.inject.Inject;
+
+/**
+ *
+ */
+public class TestViewModel extends ViewModel {
+
+    @Inject
+    public TestViewModel() {
+    }
+}
